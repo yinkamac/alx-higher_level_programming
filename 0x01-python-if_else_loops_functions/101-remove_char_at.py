e800719af3fa82ccc661c7c465e69de368b96db9
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-01-04 16:24:43 -0800

    Rechecked errors

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n < 0:
-        return str
-    count = 0
-    str_copy = ""
-    for element in str:
-        if count == n:
-            count += 1
-            continue
-        str_copy += str[count]
-        count += 1
-    return  
+    new = ""
+    i = 0
+    for c in str:
+        if i != n:
+            new += c
+        i += 1
+    return new  
