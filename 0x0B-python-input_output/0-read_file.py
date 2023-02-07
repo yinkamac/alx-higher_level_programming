05cb8770115db34c504dda5a0e2886fab66f2529
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-02-07 13:50:09 -0800

    Python Input Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Olayinka Ajibabi <yinka.ajibabi@gmail.com>
+"""
+0-read_file.py
+a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """read file utf8 con with """
+    with open(filename, encoding='UTF-8') as f:
+        for line in f:
+            print(line, end='')
