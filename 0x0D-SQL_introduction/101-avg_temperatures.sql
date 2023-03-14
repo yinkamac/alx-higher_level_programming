f8ebb1dce45ed01aa2e3f7cdfb8857902d696205
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-03-14 03:26:27 -0700

    AvgTemp

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- script that displays the average temperature (Fahrenheit) by ci-- ty ordered -- by temperature (descending).
+
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
