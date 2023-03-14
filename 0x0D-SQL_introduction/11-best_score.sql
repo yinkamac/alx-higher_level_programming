dba3b12a0f17635620e17f4177e928bd0efe4b27
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-03-14 03:17:08 -0700

    Best Score

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- script that lists all records with a score >= 10 in the table 
+-- cond_table, tables are ordered by descending score
+
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
