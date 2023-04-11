7b47b094aae0f9bd70d31a77cc5e792b218e441d
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-04-11 05:45:48 -0700

    Multi Language

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myVar = 'C is fun';
+const myBar = 'Python is cool';
+const myDar = 'JavaScript is amazing';
+
+console.log(myVar);
+console.log(myBar);
+console.log(myDar);
