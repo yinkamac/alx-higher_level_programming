3824aa62d83eb12870e7395d392cf1c8d81fdcee
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-04-17 15:03:25 -0700

    Refixed

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,23 @@
-"""
-a script that prints the State object with the name passed as
-argument from the database hbtn_0e_6_usa
-"""
+#!/usr/bin/python3
+"""script that changes the name of a State object
+from the database hbtn_0e_6_usa"""
+
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    if not state:
-        print('Not found')
-    else:
-        print(state.id)
+    states_a = session.query(State)\
+        .filter(State.name.like('%a%'))\
+        .order_by(State.id)
+    for state in states_a:
+        session.delete(state)
+    session.commit()
+    session.close()
